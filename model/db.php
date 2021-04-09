c78212ed2545c2227dcea42ef55a4b97237c0c88
Austin Salisbury <salisbury.dvlpr@gmail.com>
2021-04-09 12:23:48 -0500

    modified db.php for heroku deployment

--- model/db.php
+++ model/db.php
@@ -1,14 +1,14 @@
 <?php
     // parameters for php data object
-    /*
     $dsn = 'mysql:host=pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=iasqxmesm0vc2u5a';
     $user = 'f2qt8i85psvmhrmg';
     $password = 'lqzwcg9s6c7r2zbk';
-    */
+    /*
     $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
     $user = 'root';
     $password = '';
     $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION); // sets error mode for PDO
+    */
 
     // initialize the PDO
     try {
